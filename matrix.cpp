82aab8cbb4d7aad39dbee5e9101ca65ad51acd49
Chiku <chirantan.mitra@gmail.com>
2011-05-07 19:33:11 +0530

    Use operator overloading for mathematical oeprations on matrices

--- matrix.cpp
+++ matrix.cpp
@@ -9,22 +9,27 @@ class Matrix
       int row, col;
 
       void changeOrder(int, int);
-
+      Matrix part(int, int);
 
    public:
      	Matrix();
       Matrix(int, int);
 
-   	Matrix matAdd(Matrix, Matrix);
-      Matrix matSub(Matrix, Matrix);
-      Matrix matMul(Matrix, Matrix);
-      Matrix invert();
-
-      double element(int, int);
+   	double element(int, int);
       double determinant();
-      void getMatrix();
-      void printMatrix();
-      Matrix part(int, int);
+
+      friend Matrix operator +(Matrix, Matrix);
+      friend Matrix operator -(Matrix, Matrix);
+      friend Matrix operator *(Matrix, Matrix);
+
+      friend Matrix operator +(Matrix);
+      friend Matrix operator -(Matrix);
+
+      Matrix scalarMult(double);
+      friend Matrix operator ~(Matrix);
+
+      friend ostream& operator <<(ostream& , Matrix&);
+      friend istream& operator >>(istream& , Matrix&);
 };
 
 // no-arguement constructor
@@ -61,58 +66,6 @@ void Matrix::changeOrder(int r, int c)
 	row=r;
    col=c;
 }
-
-// matrix addition
-Matrix Matrix::matAdd(Matrix a, Matrix b)
-{
-	Matrix c;
-   if (a.row!=b.row || a.col!=b.col)
-   {
-   	cerr <<"Matries incompatible for addition.\n";
-      return c;
-   }
-   c.changeOrder(a.row, a.col);
-   for (int i=0; i<a.row; i++)
-   	for (int j=0; j<a.col; j++)
-      	c.mat[i][j]=a.element(i, j)+b.element(i, j);
-   return c;
-}
-
-// matrix subtraction
-Matrix Matrix::matSub(Matrix A, Matrix B)
-{
-	Matrix C;
-   if (A.row!=B.row || A.col!=B.col)
-   {
-   	cerr <<"Matries incompatible for subtraction.\n";
-      return C;
-   }
-   C.changeOrder(A.row, A.col);
-   for (int i=0; i<A.row; i++)
-   	for (int j=0; j<A.col; j++)
-      	C.mat[i][j]=A.element(i, j)-B.element(i, j);
-   return C;
-}
-
-// matrix multiplication
-Matrix Matrix::matMul(Matrix A, Matrix B)
-{
-	Matrix C;
-   if (A.col!=B.row)
-   {
-   	cerr <<"Matries incpmpatible for multiplication.\n";
-      return C;
-   }
-   C.changeOrder(A.row, B.col);
-   int i, j, k;
-   for (i=0; i<A.row; i++)
-	  	for (j=0; j<B.col; j++)
-        	for (k=0; k<A.col; k++)
-         	C.mat[i][j] += A.element(i, k) * B.element(i, j);
-
-   return C;
-}
-
 
 // determines corresponding determinant
 double Matrix::determinant()
@@ -154,26 +107,6 @@ double Matrix::determinant()
 	return d;
 }
 
-// accept matrix from console
-void Matrix::getMatrix()
-{
-	for (int i=0; i<row; i++)
-      for (int j=0; j<col; j++)
-      	cin >>mat[i][j];
-}
-
-// prints matrix to console
-void Matrix::printMatrix()
-{
-	for (int i=0; i<row; i++)
-   {
-      cout <<endl;
-      for (int j=0; j<col; j++)
-      	cout <<mat[i][j] <<'\t';
-   }
-}
-
-
 // used in finding the inverse of matrix
 Matrix Matrix::part(int m, int n)
 {
@@ -197,25 +130,105 @@ Matrix Matrix::part(int m, int n)
 }
 
 
-Matrix Matrix::invert()
+// matrix addition
+Matrix operator +(Matrix a, Matrix b)
 {
-	double detA=determinant();
+	Matrix c;
+   if (a.row!=b.row || a.col!=b.col)
+   {
+   	cerr <<"Matries incompatible for addition.\n";
+      return c;
+   }
+   c.changeOrder(a.row, a.col);
+   for (int i=0; i<a.row; i++)
+   	for (int j=0; j<a.col; j++)
+      	c.mat[i][j]=a.element(i, j)+b.element(i, j);
+   return c;
+}
+
+// matrix subtraction
+Matrix operator -(Matrix A, Matrix B)
+{
+	Matrix C;
+   if (A.row!=B.row || A.col!=B.col)
+   {
+   	cerr <<"Matries incompatible for subtraction.\n";
+      return C;
+   }
+   C.changeOrder(A.row, A.col);
+   for (int i=0; i<A.row; i++)
+   	for (int j=0; j<A.col; j++)
+      	C.mat[i][j]=A.element(i, j)-B.element(i, j);
+   return C;
+}
+
+// matrix multiplication
+Matrix operator *(Matrix A, Matrix B)
+{
+	Matrix C;
+   if (A.col!=B.row)
+   {
+   	cerr <<"Matries incpmpatible for multiplication.\n";
+      return C;
+   }
+   C.changeOrder(A.row, B.col);
+   int i, j, k;
+   for (i=0; i<A.row; i++)
+	  	for (j=0; j<B.col; j++)
+        	for (k=0; k<A.col; k++)
+         	C.mat[i][j] += A.element(i, k) * B.element(i, j);
+
+   return C;
+}
+
+// unary +
+Matrix operator +(Matrix A)
+{
+	return A;
+}
+
+// unary -
+Matrix operator -(Matrix A)
+{
+	for (int i=0; i<A.row; i++)
+   	for (int j=0; j<A.col; j++)
+      	A.mat[i][j] = -A.mat[i][j];
+	return A;
+}
+
+// scalar multiplicaiton
+Matrix Matrix::scalarMult(double x)
+{
+   Matrix B;
+   B.changeOrder(row, col);
+	for (int i=0; i<row; i++)
+   	for (int j=0; j<col; j++)
+      	B.mat[i][j] = x*mat[i][j];
+	return B;
+}
+
+
+
+// finds the inverse of the matrix
+Matrix operator ~(Matrix A)
+{
+	double detA=A.determinant();
 	Matrix temp;
 	int i, j;
-	if (detA == 0 || row!=col)
+	if (detA == 0 || A.row!=A.col)
 	{
 		cout <<"The matrix has no inverse!\n";
 		return temp;
 	}
-   temp.changeOrder(row, col);
-   int n=row;
+   temp.changeOrder(A.row, A.col);
+   int n=A.row;
 
 	for (i=0; i<n; i++)
 		for (j=0; j<n; j++)
 		{
 			double t;
          Matrix aux(n, n);
-			aux=part(i, j);
+			aux=A.part(i, j);
 			t=aux.determinant();
 			temp.mat[j][i] = t/detA;
 			if ((i+j)%2)
@@ -224,18 +237,39 @@ Matrix Matrix::invert()
    return temp;
 }
 
+// accept matrix from console
+istream& operator >>(istream& s, Matrix& A)
+{
+	for (int i=0; i<A.row; i++)
+      for (int j=0; j<A.col; j++)
+      	s >>A.mat[i][j];
+   return s;
+}
+
+// prints matrix to console
+ostream& operator <<(ostream& s, Matrix& A)
+{
+	for (int i=0; i<A.row; i++)
+   {
+      s <<endl;
+      for (int j=0; j<A.col; j++)
+      	s <<A.mat[i][j] <<'\t';
+   }
+   return s;
+}
 
 
 void main()
 {
    Matrix A(3, 3);
-   A.getMatrix();
-   A.printMatrix();
+   cin >>A;
+   cout <<A;
    cout <<endl;
    cout <<"Determinant = " <<A.determinant() <<endl;
-   Matrix C(3, 3);
-   C=A.invert();
-   C.printMatrix();
+   A=-A.scalarMult(3);
+	cout <<A;
+
    int a;
    cin >>a;
 }
+
