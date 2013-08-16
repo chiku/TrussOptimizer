24eee08be2ffdeb30ac97e1adf850b7bfd011f74
Chiku <chirantan.mitra@gmail.com>
2013-08-16 23:54:46 +0530

    Don't test third-party cmatrix

--- matrix.cpp
+++ matrix.cpp
@@ -11,23 +11,3 @@ using namespace std;
 typedef CMatrix::Matrix<double> Matrix;
 
 #endif
-
-/*
-int main()
-{
-	Matrix A;
-	A.setSize(3, 3);
-	A(0, 0) = 1; A(0, 1) = 3; A(0, 2) = 4;
-	A(1, 0) = 3; A(1, 1) = 2; A(1, 2) = 1;
-	A(2, 0) = 7; A(2, 1) = 8; A(2, 2) = 8;
-	cout <<A;
-	Matrix B = A;
-
-	B.invert();
-	cout <<endl <<B;
-	Matrix C;
-	C = A*B;
-	cout <<endl <<C;
-	cout <<endl <<B.rows() <<B.columns();
-}
-*/
